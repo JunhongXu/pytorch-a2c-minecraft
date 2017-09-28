8b02ca9842911120654567842a10b839ee045e43
jxu7 <jxu7@bsu.edu>
2017-09-28 17:13:19 -0400

    first commit

---
+++ policies.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class CNNPolicy(nn.Module):
+    def __init__(self, obs_space, num_actions):
+        """64*3*3 -> 128*3*3 -> 256*3*3 -> 512"""
+        super(CNNPolicy, self).__init__()
+        h, w = obs_space
+        self.obs_space = obs_space
+        self.num_actions = num_actions
+
+        self.conv1 = self.__make_conv_elu(3, 32, 8, 4)
+        self.conv2 = self.__make_conv_elu(32, 64, 4, 2)
+        self.conv3 = self.__make_conv_elu(64, 128, 3, 1)
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(256*(h//8), 512),
+            nn.ELU()
+        )
+
+        self.value = nn.Linear(512, 1)
+        self.action = nn.Linear(512, num_actions)
+
+        self.init_weight()
+
+    def init_weight(self):
+        for module in self.modules():
+            print(isinstance(module, nn.Conv2d))
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+
+    @staticmethod
+    def __make_conv_elu(input_feats, output_feats, size, stride, padding=0):
+        return nn.Sequential(nn.Conv2d(input_feats, output_feats, kernel_size=size, stride=stride, padding=padding), nn.ELU())
+
+if __name__ == '__main__':
+    cnn = CNNPolicy((84, 84), 4)
+
